c6bf57ece97190c83f9a1036d5d3655c9227df7f
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-04-29 14:53:50 +0300

    new

--- cats/views.py
+++ cats/views.py
@@ -25,35 +25,3 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
-
-
-
-
-
-
-
-
-
-#  импортируйте в код всё необходимое
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-
-from .models import Post
-from .serializers import PostSerializer
-
-
-@api_view(['GET', 'POST'])
-def api_posts(request):
-    if request.method == 'POST':
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
-
