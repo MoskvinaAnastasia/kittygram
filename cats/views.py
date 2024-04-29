147e7358d30bc80aedbc4a7936c5cb415e66b016
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-04-29 14:51:46 +0300

    new

--- cats/views.py
+++ cats/views.py
@@ -9,9 +9,8 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        # Создаём объект сериализатора
-        # и передаём в него данные из POST-запроса
-        serializer = CatSerializer(data=request.data)
+        # В случае POST-запроса добавим список записей в БД
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             # Если полученные данные валидны —
             # сохраняем данные в базу через save().
@@ -22,3 +21,39 @@ def cat_list(request):
         # Если данные не прошли валидацию — 
         # возвращаем информацию об ошибках и соответствующий статус-код:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # В случае GET-запроса возвращаем список всех котиков
+    cats = Cat.objects.all()
+    serializer = CatSerializer(cats, many=True)
+    return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+#  импортируйте в код всё необходимое
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+@api_view(['GET', 'POST'])
+def api_posts(request):
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
